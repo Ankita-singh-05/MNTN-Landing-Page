b87e83fdf8590cab94b0f97f0ffda51dfc4d1542
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-02-24 12:41:10 +0530

    Footer Component

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -4,13 +4,22 @@ function Footer() {
   return (
     <div className="flex justify-between m-14">
       <div>
-        <h1>MNTN</h1>
-        <p>Get out there & discover your next slope, mountain & destination!</p>
-        <p>Copyright 2019 MNTN, Inc. Terms & Privacy</p>
+        <h1 className="mb-4 font-[Chronicle Display] tracking-[0.01em] text-[32px]">MNTN</h1>
+        <div className="font-[Gilroy] text-[18px]">
+          <p className="w-56 font-bold">
+            Get out there & discover your next slope, mountain & destination!
+          </p>
+          <p className="mt-[6em] font-medium opacity-[0.5]">
+            Copyright 2019 MNTN, Inc. Terms & Privacy
+          </p>
+        </div>
       </div>
-      <div>
-        More on The Blog
-        <div className="list-none">
+
+      <div className="font-[Gilroy]">
+        <h1 className="text-[#FBD784] mb-4 font-bold text-[24px]">
+          More on The Blog
+        </h1>
+        <div className="list-none text-[18px] space-y-4 font-medium">
           <li>About MNTN</li>
           <li>Contributors & Writers</li>
           <li>Write For Us</li>
@@ -18,9 +27,12 @@ function Footer() {
           <li>Privacy Policy</li>
         </div>
       </div>
-      <div className="">
-        More on MNTN
-        <div className="list-none">
+
+      <div className="font-[Gilroy]">
+        <h1 className="text-[#FBD784] mb-4 font-bold text-[24px]">
+          More on MNTN
+        </h1>
+        <div className="list-none text-[18px] space-y-4 font-medium">
           <li>The Team</li>
           <li>Jobs</li>
           <li>Press</li>
