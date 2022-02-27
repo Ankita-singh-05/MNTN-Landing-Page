2a7f06cdaa66a3d08d6c55ef44c094b15253eceb
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-02-27 14:33:34 +0530

    hero and navbar styling

--- src/components/hero/hero.components.js
+++ src/components/hero/hero.components.js
@@ -64,13 +64,13 @@ function HeroComponent() {
       </div>
 
       {/* Get Started Component */}
-      <div className="relative mt-[13rem]">
-    <img src={VgImage.vgImage} className="vg-image"></img>
+      <div className="relative mt-[13rem] sm:mt-[9.4rem] md:mt-[11.3rem]">
+    <img src={VgImage.vgImage} className="vg-image "></img>
   </div>
       <div className="content1-container flex ml-14 gap-4">
         <div className="mt-12">
           <div className="flex">
-            <div className="w-11 h-0.5 left-0 bg-[#FBD784] mt-[8.2px]"></div>
+            <div className="w-11 h-0.5 left-0 bg-[#FBD784] mt-[5px]"></div>
             <h3 className="ml-4 text-[#FBD784] font-[Gilroy] font-extrabold text-[18px] tracking-[6px]">GET STARTED</h3>
           </div>
           <h1 className="text-[#fff] font-semibold text-[64px] font-[Chronicle Display] w-[10em]">What level of hiker are you?</h1>
--- src/components/navbar/navbar.component.js
+++ src/components/navbar/navbar.component.js
@@ -1,23 +1,17 @@
 import React from "react";
 import { VscAccount } from "react-icons/vsc";
 
-// Icons
-import { IoLogoInstagram } from "react-icons/io";
-import { FiTwitter } from "react-icons/fi";
-import { HiOutlineArrowNarrowDown } from "react-icons/hi";
-
 // Background
 import CloudImage from "../images/cloud";
 
 function Navbar() {
   return (
     <>
-<div className="absolute">
+<div className="absolute cloud-image-container ">
     <img src={CloudImage.cloudImage} alt="Clouds" className="cloud-image"></img>
     </div>
 
-      <div className="navbar-toggler relative">
-        <nav className="flex justify-between ">
+        <nav className="flex justify-between navbar-toggler relative">
           <div className="font-[Chronicle Display] text-[32px] text-white tracking-[0.01em] m-6">
             MNTN
           </div>
@@ -35,7 +29,7 @@ function Navbar() {
             Account
           </div>
         </nav>
-      </div>
+
     </>
   );
 }
