01ac9c217211a13b414bc6b9d8abd46e4df4f2d1
Ankita-singh-05 <asankitasingh2017@gmail.com>
2022-02-27 12:36:38 +0530

    hero and navbar

--- src/components/navbar/navbar.component.js
+++ src/components/navbar/navbar.component.js
@@ -18,9 +18,10 @@ function Navbar() {
 
       <div className="navbar-toggler relative">
         <nav className="flex justify-between ">
-          <div className="font-[Chronicle Display] text-[32px] text-white tracking-[0.01em] m-8">
+          <div className="font-[Chronicle Display] text-[32px] text-white tracking-[0.01em] m-6">
             MNTN
           </div>
+          
           <div
             className="list-none flex space-x-5 text-[18px] text-white font-[
             Gilroy] font-semibold m-8"
@@ -30,7 +31,7 @@ function Navbar() {
             <li>Blog</li>
           </div>
           <div className="flex gap-2 text-[18px] text-white font-[Gilroy] m-8">
-            <VscAccount className="mt-1" />
+            <VscAccount className="" />
             Account
           </div>
         </nav>
